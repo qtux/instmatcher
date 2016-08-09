8d98aa73bb55f7b12348bb4717594a25901ea628
Matthias Gazzari <matthias.gazzari@cern.ch>
2016-08-09 18:06:07 +0200

    update geo module tests to reflect changes

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -13,33 +13,76 @@
 # limitations under the License.
 
 import unittest
+from instmatcher.geo import init, geocode, geocodeAll
+
 import os
-from instmatcher.geo import init, geocode
+from itertools import zip_longest
+
+cases = {
+	(None, None): [],
+	('x', None): [],
+	('Turyan', None): [],
+	('Klin Geriatr, Hamburg', 'DE'): [],
+	('Sao Paulo',	None): [(-23.5475, -46.63611)],
+	('São Paulo',	None): [(-23.5475, -46.63611)],
+	('São Paulo',	'BR'): [(-23.5475, -46.63611)],
+	('Sao Paulo',	'BR'): [(-23.5475, -46.63611)],
+	('Xian', None): [(34.25833, 108.92861)],
+	('Xian', 'CN'): [(34.25833, 108.92861)],
+	('Gutao', None): [(37.2025, 112.17806)],
+	('Gutao', 'CN'): [(37.2025, 112.17806)],
+	('Boston', None): [
+		(42.35843, -71.05977),
+		(52.97633, -0.02664),
+		(7.87111, 126.36417),
+		(30.79186, -83.78989),
+	],
+	('Boston', 'US'): [
+		(42.35843, -71.05977),
+		(30.79186, -83.78989),
+	],
+	('London', None): [
+		(51.50853, -0.12574),
+		(42.98339, -81.23304),
+		(37.12898, -84.08326),
+		(39.88645, -83.44825),
+		(51.51279, -0.09184),
+		(36.47606, -119.44318),
+		(35.32897, -93.25296),
+		(1.98487, -157.47502),
+	],
+	('London', 'GB'): [
+		(51.50853, -0.12574),
+		(51.51279, -0.09184),
+	],
+	('London', 'CA'): [(42.98339, -81.23304)],
+}
 
 class test_geo(unittest.TestCase):
 	
 	def setUp(self):
 		init(procs=os.cpu_count(), multisegment=True)
 	
-	def test_cases(self):
-		cases = {
-			(None, None): (None, None),
-			('x', None): (None, None),
-			('Turyan', None): (None, None),
-			('Klin Geriatr, Hamburg', 'DE'): (None, None),
-			('Sao Paulo',	None): (-23.5475, -46.63611),
-			('São Paulo',	None): (-23.5475, -46.63611),
-			('São Paulo',	'BR'): (-23.5475, -46.63611),
-			('Sao Paulo',	'BR'): (-23.5475, -46.63611),
-			('Xian', None): (34.25833, 108.92861),
-			('Xian', 'CN'): (34.25833, 108.92861),
-			('Gutao', None): (37.2025, 112.17806),
-			('Gutao', 'CN'): (37.2025, 112.17806),
-			('Boston', None): (42.35843, -71.05977),
-			('Boston', 'US'): (42.35843, -71.05977),
-			('London', None): (51.50853, -0.12574),
-			('London', 'GB'): (51.50853, -0.12574),
-			('London', 'CA'): (42.98339, -81.23304),
-		}
-		for args, result in cases.items():
-			self.assertEqual(geocode(*args), result, msg=args)
+	def test_geocode(self):
+		for args, targets in cases.items():
+			coords = geocode(*args)
+			for target in targets:
+				self.assertEqual(coords, target, msg=args)
+				break
+			else:
+				self.assertEqual(coords, (None, None), msg=args)
+	
+	def test_geocodeAll(self):
+		for args, targets in cases.items():
+			coords = geocodeAll(*args)
+			for result, target in zip_longest(coords, targets):
+				self.assertEqual(result, target, msg=args)
+
+if __name__ == '__main__':
+	init(procs=os.cpu_count(), multisegment=True)
+	for query, coords in cases.items():
+		coords = geocodeAll(*query)
+		print('', query,': [', sep='')
+		for latlon in coords:
+			print('\t', latlon, ',', sep='')
+		print('],')
