7e29432d7d24f6a9401c1e4c96f117c7f1f09e61
Matthias Gazzari <matthias.gazzari@cern.ch>
2016-08-10 13:49:28 +0200

    remove unused variable

--- tests/test_core.py
+++ tests/test_core.py
@@ -372,7 +372,7 @@ if __name__ == '__main__':
 	}
 	for name, item in dicts.items():
 		print(name + ' = {')
-		for args, targets in item.items():
+		for args in item.keys():
 			institutes = core.queryAll(*args)
 			print('\t', args,': [', sep='')
 			for result in institutes:
