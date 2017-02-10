8481ac041a5a12ac3734d4ad95a77d281828a2ea
Matthias Gazzari <matthias.gazzari@cern.ch>
2017-02-10 18:41:58 +0100

    replace the extract and find functions in the API - replace their functionality with the functions provided in the core,   parser and geo modules - remove the corresponding tests - change the match function to be a derivation from the new matchAll   function which yields every possible institution given the search   parameters

--- instmatcher/__init__.py
+++ instmatcher/__init__.py
@@ -20,109 +20,44 @@ means to extract information using either the internal geocoder along
 with an external grobid server or by providing self-defined functions.
 '''
 
-from .core import query, expandAbbreviations
-from .geo import geocode
-from .parser import parse
+from .core import findAll, find
+from .geo import geocodeAll, geocode
+from .parser import parseAll, parse
 from .version import __version__
-from functools import partial
 
-def _appendDoc(docstring):
-	def decorator(function):
-		function.__doc__ += docstring
-		return function
-	return decorator
-
-############################# create grobid parser #############################
-def createGrobidParser(url='http://0.0.0.0:8080'):
+def matchAll(string, url='http://0.0.0.0:8080', offset=1):
 	'''
-	Create a parser function using a grobid server to extract data from
-	affiliation strings.
-	:param url: the URL to the grobid service
-	'''
-	return partial(parser.parse, url=url)
-
-############################### find functions ################################
-_find_doc = '''
-	Applying the ISO 3166-1 alpha-2 country code will restrict the
-	search results to the given country.
-	The latitude, longitude and offset (in degree of arcs) parameters
-	describe a geographical box in which results are preferred. Note that
-	an offset of one degree of arc corresponds to about 111 km: Using an
-	offset of 1 results into a box with a width of approximately 222 km.
+	Yield all institutions matching the affiliation string using a
+	grobid service to parse the string.
 	
-	:param institution: the list of institutions to search for
-	:param alpha2: the country to restrict search results to
-	:param lat: the latitude describing the middle of the preferred box
-	:param lon: the longitude describing the middle of preferred box
+	:param string: the affiliation string to be extracted
+	:param url: the URL to the grobid service
 	:param offset: the half-width of the preferred box in degree of arcs
-	:param ignore: ignored keyword arguments
-'''
+	'''
+	for parsed in parseAll(string, url):
+		alpha2 = parsed.get('alpha2')
+		for settlement in parsed['settlement']:
+			for coord in geocodeAll(settlement, alpha2):
+				institutions = findAll(
+					institution=parsed.get('institution'),
+					alpha2=alpha2,
+					lat=coord.get('lat'),
+					lon=coord.get('lon'),
+					offset=offset
+				)
+				for instit in institutions:
+					yield instit
 
-@_appendDoc(_find_doc)
-def findAll(institution, alpha2=None, lat=None, lon=None, offset=0.4, **ignore):
+def match(string, url='http://0.0.0.0:8080', offset=1):
 	'''
-	Yield all institutions compatible to the search parameters sorted in
-	descending order starting with the most accurate search result.
-	'''
-	if not isinstance(institution, list):
-		return
-	for inst in institution:
-		fullName = expandAbbreviations(inst)
-		for result in query(fullName, alpha2, lat, lon, offset):
-			yield result[0]
-
-@_appendDoc(_find_doc)
-def find(institution, alpha2=None, lat=None, lon=None, offset=0.4, **ignore):
-	'''
-	Find the most accurate institution described by the search parameters.
+	Find the most accurate institution matching the affiliation string
+	using a grobid service to parse the string.
+	
+	:param string: the affiliation string to be extracted
+	:param url: the URL to the grobid service
+	:param offset: the half-width of the preferred box in degree of arcs
 	'''
 	try:
-		return next(findAll(institution, alpha2, lat, lon, offset))
+		return next(matchAll(string, url, offset))
 	except StopIteration:
 		return
-
-############################## extract functions ##############################
-_extract_doc = '''
-	:param string: the affiliation string to be extracted
-	:param parse: a function to parse the affiliation string
-	:param geocode: a function to retrieve coordinates of a settlement
-'''
-
-@_appendDoc(_extract_doc)
-def extractAll(string, parse=createGrobidParser(), geocode=geocode):
-	'''
-	Yield the data extracted from the affiliation string augmented with
-	each compatible geographical location. The generator is sorted in a
-	descending order starting with the most likely coordinate.
-	'''
-	affiDict = parse(string)
-	emptyGenerator = True
-	for settlement in affiDict['settlement']:
-		for position in geocode(settlement, affiDict['alpha2']):
-			emptyGenerator = False
-			result = affiDict.copy()
-			result.update(position)
-			yield result
-	if emptyGenerator:
-		yield affiDict
-
-@_appendDoc(_extract_doc)
-def extract(string, parse=createGrobidParser(), geocode=geocode):
-	'''
-	Extract the data from the given affiliation string and augment it
-	with the most likely geographical location.
-	'''
-	return next(extractAll(string, parse, geocode))
-
-############################### match function ################################
-
-def match(string, parse=createGrobidParser(), geocode=geocode, offset=1):
-	'''
-	Find the most accurate institution matching the affiliation string.
-	
-	:param string: the affiliation string to be extracted
-	:param parse: a function to parse the string into a structure
-	:param geocode: a function to retrieve coordinates of a settlement
-	:param offset: the half-width of the preferred box in degree of arcs
-	'''
-	return find(offset=offset, **extract(string, parse, geocode))
